9e2f082d35ef4188812c0b91d020ab3d81c5fcf8
Szymon Rybczak <szymon.rybczak@gmail.com>
2021-08-22 18:19:00 +0200

    Add dropdown alert manager

--- App.tsx
+++ App.tsx
@@ -1,6 +1,20 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, StyleSheet } from 'react-native'
+import DropdownAlert from 'react-native-dropdownalert'
+import AppAlertManager from './src/helpers/AppAlertManager'
 
-const App = () => <View />
+const App = () => (
+    <View style={styles.container}>
+        <DropdownAlert
+            ref={(ref: DropdownAlert) => AppAlertManager.setDropdown(ref)}
+        />
+    </View>
+)
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+})
 
 export default App
